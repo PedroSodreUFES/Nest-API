5add126697f417a404c356254bd6c3fe41d45637
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-04-28 22:54:56 -0300

    feat: setting jwt guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { Env } from 'src/env'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
@@ -10,16 +11,18 @@ import { Env } from 'src/env'
     JwtModule.registerAsync({
       inject: [ConfigService],
       global: true,
-      async useFactory(config: ConfigService<Env, true>) {
+      useFactory(config: ConfigService<Env, true>) {
         const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
         const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
         return {
-          privateKey,
-          publicKey,
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
         }
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
